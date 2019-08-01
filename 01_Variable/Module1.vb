3176f6f4dd55a12045ef79a215b4a9c782d4d488
K1rbi <171025@trinityschools.nz>
2019-08-01 19:32:34 +1200

    Variables :feelsgood:

--- 01_Variable/Module1.vb
+++ 01_Variable/Module1.vb
@@ -3,7 +3,26 @@
     Sub Main()
         Const MYMESSAGE As String = "I heart Computer Science!"
 
+        Const intMyAge As String = "15"
+
         Dim Var14b13 As String = "Weird var name"
+
+        Dim strMyName As String = "Tom Kavanagh"
+
+        Dim dblDouble As Double = "4.567"
+
+        Console.WriteLine(MYMESSAGE)
+
+        Console.WriteLine(Var14b13)
+
+        Console.WriteLine(strMyName)
+
+        Console.WriteLine(intMyAge)
+
+
+
+        Console.ReadLine()
+
 
     End Sub
 
